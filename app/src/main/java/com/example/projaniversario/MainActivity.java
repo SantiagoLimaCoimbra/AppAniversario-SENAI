1dcafee57fd31a4e8d51b0e0a4d98539d5271c09
SantiagoLimaCoimbra <santiagolimacoimbra@gmail.com>
2021-06-27 04:05:45 -0300

    Atualização: 27/06 - Intregação com o Firebase mal sucedida.

--- app/src/main/java/com/example/projaniversario/MainActivity.java
+++ app/src/main/java/com/example/projaniversario/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.projaniversario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText inputData;
+    public Button cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            inputData = findViewById(R.id.inputData);
+            cadastrar = findViewById(R.id.botao1);
+
+            cadastrar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, aniversarios.class);
+                    startActivity(intent);
+                }
+            });
     }
 }
--- app/src/main/java/com/example/projaniversario/aniversarios.java
+++ app/src/main/java/com/example/projaniversario/aniversarios.java
@@ -3,12 +3,23 @@ package com.example.projaniversario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class aniversarios extends AppCompatActivity {
+
+    public TextView dataNiver1;
+    public TextView dataNiver2;
+    public TextView dataNiver3;
+    public TextView dataNiver4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aniversarios);
+
+        dataNiver1 = findViewById(R.id.aniver1);
+        dataNiver2 = findViewById(R.id.aniver2);
+        dataNiver3 = findViewById(R.id.aniver3);
+        dataNiver4 = findViewById(R.id.aniver4);
     }
 }
